bb9e56e5195935a46dda9b40d55f6c17c5563843
Sebastian Rzeszutko <rzeszutkoseb@gmail.com>
2025-11-18 20:53:10 +0100

    Add json-server

--- server.mjs
+++ server.mjs
@@ -1,17 +1,25 @@
-import path from 'path';
 import jsonServer from 'json-server';
 
 const server = jsonServer.create();
-const router = jsonServer.router(path.join('dist', 'db', 'app.json'));
+const router = jsonServer.router('build/db/app.json');
+
 const middlewares = jsonServer.defaults({
-  static: 'dist',
-  noCors: true
+  static: 'build',
+  noCors: true,
 });
+
 const port = process.env.PORT || 3131;
 
 server.use(middlewares);
+
+server.use(
+  jsonServer.rewriter({
+    '/api/*': '/$1',
+  })
+);
+
 server.use(router);
 
-server.listen(port);
-
-export default server;
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
